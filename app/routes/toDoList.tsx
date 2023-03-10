034c9c07b9924d4579d0bc104df29ee17453c65c
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-10 10:28:00 +0545

    added css in home.css and deleted todos file

--- app/routes/toDoList.tsx
+++ app/routes/toDoList.tsx
@@ -1,13 +1,31 @@
-import Todos, {links as newTodoLinks} from "~/components/todos";
+import homeStyles from '~/styles/home.css';
 
 export default function toDoList() {
   return (
-    <main>
-      <Todos />
-    </main>
+    <>
+    <form action="post" id="todos-form">
+      <p>
+        <input type="text" id="title" name="title" placeholder="Title here" required/>
+      </p>
+      <p>
+        <input type="text" id="title" name="title" placeholder="Write something here..." required/>
+      </p>
+      <div className="form-actions">
+        <button>Add Todos</button>
+      </div>
+    </form>
+    <div id="my-div">
+      <h1>Title</h1>
+      <span>Your Todos here</span>
+    </div>
+    </>
   )
 }
 
+export function action(){
+  
+}
+
 export function links(){
- return [...newTodoLinks()];
+  return [{rel:'stylesheet', href: homeStyles}]
 }
