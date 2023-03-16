5ba960b2dbc6f3632e651d2508a05d40a6ec0a8a
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-16 11:15:38 +0545

    adding test data inside our blog table

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+const db = new PrismaClient();
+
+async function seed(){
+    await Promise.all(
+        gettoDoList().map((todos)=>{
+            return db.blog.create({data:todos});
+        })
+    )
+}
+
+seed();
+
+function gettoDoList(){
+    return[
+        {
+            author_name:"Aniket Tamrakar",
+            article_title:'Git Aliasing',
+            published_date: new Date("2022-12-01").toISOString(),
+
+        }
+    ]
+}
