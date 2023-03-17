4cfe72a91b6e002a84d69a33a4f2067a2ffaf351
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-17 09:44:20 +0545

    added datetime object and redesigning $blogId.tsx file

--- app/routes/blog/$blogId.tsx
+++ app/routes/blog/$blogId.tsx
@@ -1,6 +1,64 @@
+// import type { LoaderArgs } from "@remix-run/node";
+// import { json } from "@remix-run/node";
+// import { Link, useLoaderData } from "@remix-run/react";
+// import styles from '~/styles/newBlog.css';
+
+// import { db } from "~/utils/db.server";
+
+// export const loader = async ({ params }: LoaderArgs) => {
+//   const blogs = await db.blog.findUnique({
+//     where: { id: params.blogId },
+//   });
+//   if (!blogs) {
+//     throw new Error("Joke not found");
+//   }
+//   return json({ blogs });
+// };
+
+// export default function BlogRoute() {
+//   const data = useLoaderData<typeof loader>();
+
+//   return (
+//     // <div>
+//     //   <p>{data.blogs.author_name}</p>
+//     //   <p>{data.blogs.published_date}</p>
+//     //   <Link to=".">{data.blogs.article_title}</Link>
+//     // </div>
+//     <ul id="note-list">
+//       <li key={data.blogs.id} className="note">
+//         <article>
+//           <header>
+//             <ul className="note-meta">
+//               <li>
+//                 {/* <time dateTime={data.blogs.id}>
+//                   {new Date(data.blogs.id).toLocaleDateString('en-US', {
+//                     day: 'numeric',
+//                     month: 'short',
+//                     year: 'numeric',
+//                     hour: '2-digit',
+//                     minute: '2-digit',
+//                   })}
+//                 </time> */}
+//                 <h2>{data.blogs.published_date?.slice(0.8)}</h2>
+//               </li>
+//             </ul>
+//             <h2>{data.blogs.author_name}</h2>
+//           </header>
+//           <p>{data.blogs.article_title}</p>
+//         </article>
+//       </li>
+//   </ul>
+//   );
+// }
+
+// export function links() {
+//   return [{ rel: 'stylesheet', href: styles }];
+// }
+
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import styles from '~/styles/newBlog.css';
 
 import { db } from "~/utils/db.server";
 
@@ -17,11 +75,33 @@ export const loader = async ({ params }: LoaderArgs) => {
 export default function BlogRoute() {
   const data = useLoaderData<typeof loader>();
 
+  const currentDate = new Date().toLocaleString('en-US', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
   return (
-    <div>
-      <p>{data.blogs.author_name}</p>
-      <p>{data.blogs.published_date}</p>
-      <Link to=".">{data.blogs.article_title} Permalink</Link>
-    </div>
+    <ul id="note-list">
+      <li key={data.blogs.id} className="note">
+        <article>
+          <header>
+            <ul className="note-meta">
+              <li>
+                <time dateTime={currentDate}>{currentDate}</time>
+              </li>
+            </ul>
+            <h2>{data.blogs.author_name}</h2>
+          </header>
+          <p>{data.blogs.article_title}</p>
+        </article>
+      </li>
+    </ul>
   );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
 }
