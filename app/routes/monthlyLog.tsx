d93ebc5e863a718b0ea5ba708e8db3aa8926fc66
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-21 13:55:14 +0545

    trying to show data upto 28 days from the start date

--- app/routes/monthlyLog.tsx
+++ app/routes/monthlyLog.tsx
@@ -1,45 +1,46 @@
-import { db } from '~/utils/db.server';
-import { useLoaderData } from '@remix-run/react';
+import { db } from "~/utils/db.server";
+import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 
 const DAYS_AGO = 28;
 
 export const loader = async () => {
-  const cutoffDate = new Date();
+  const cutoffDate = new Date("2023/03/19");
+
   cutoffDate.setDate(cutoffDate.getDate() - DAYS_AGO);
 
   return json({
     blogPosts: await db.blog.findMany({
       where: {
         published_date: {
-          gte: cutoffDate
-        }
+          gte: cutoffDate,
+        },
       },
       select: {
         author_name: true,
         article_title: true,
-        published_date: true
+        published_date: true,
       },
       orderBy: {
-        currentCycle: "desc"
-      }
-    })
+        currentCycle: "desc",
+      },
+    }),
   });
 };
 
 export default function MonthlyLog() {
   const data = useLoaderData<typeof loader>();
-  
+
   return (
     <section className="monthly-log">
       <div className="container">
-        <h1 style={{color:'white'}}>Below are the monthly log</h1>
+        <h1 style={{ color: "white" }}>Below are the monthly log</h1>
         {data.blogPosts.map((post, index) => (
           <div key={index}>
-            <h2 style={{color:'green'}}>{post.article_title}</h2>
+            <h2 style={{ color: "green" }}>{post.article_title}</h2>
             <p>{post.author_name}</p>
             {post.published_date ? (
-              <p>{post.published_date.toLocaleString()}</p>
+              <p>{post.published_date.toLocaleString().slice(0, 10)}</p>
             ) : (
               <p>No published date available</p>
             )}
