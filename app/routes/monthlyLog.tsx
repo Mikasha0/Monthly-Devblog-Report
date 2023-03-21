4a778b0facfc230788a6aeed52d78696c9420503
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-21 09:03:34 +0545

    adding loader function in the monthlyLog.tsx file

--- app/routes/monthlyLog.tsx
+++ app/routes/monthlyLog.tsx
@@ -1,5 +1,19 @@
+import {db} from '~/utils/db.server';
+import { useLoaderData } from '@remix-run/react';
+import { json } from "@remix-run/node";
 
-export default function monthlyLog() {
+
+export const loader = async () => {
+    return json({
+      blogs: await db.blog.findMany({take: 5,
+        select: { id: true,author_name:true },
+        distinct: ["author_name"],
+        orderBy: { author_name: "asc" },}),
+    });
+  };
+
+export default function MonthlyLog() {
+    const data = useLoaderData<typeof loader>();
   return (
     <section className="monthly-log">
         <div className="container">
