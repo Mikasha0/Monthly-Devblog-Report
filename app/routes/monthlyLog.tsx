b565c8e7b5d445b99386f91d83850204c6ad82d6
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-21 10:35:52 +0545

    showing author_name, article_title and published_date of all the users

--- app/routes/monthlyLog.tsx
+++ app/routes/monthlyLog.tsx
@@ -1,24 +1,51 @@
-import {db} from '~/utils/db.server';
+import { db } from '~/utils/db.server';
 import { useLoaderData } from '@remix-run/react';
 import { json } from "@remix-run/node";
 
+const DAYS_AGO = 28;
 
 export const loader = async () => {
-    return json({
-      blogs: await db.blog.findMany({take: 5,
-        select: { id: true,author_name:true },
-        distinct: ["author_name"],
-        orderBy: { author_name: "asc" },}),
-    });
-  };
+  const cutoffDate = new Date();
+  cutoffDate.setDate(cutoffDate.getDate() - DAYS_AGO);
+
+  return json({
+    blogPosts: await db.blog.findMany({
+      where: {
+        published_date: {
+          gte: cutoffDate
+        }
+      },
+      select: {
+        author_name: true,
+        article_title: true,
+        published_date: true
+      },
+      orderBy: {
+        currentCycle: "desc"
+      }
+    })
+  });
+};
 
 export default function MonthlyLog() {
-    const data = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>();
+  
   return (
     <section className="monthly-log">
-        <div className="container">
-            <h1 style={{color:'white'}}>Below are the monthly log</h1>
-        </div>
+      <div className="container">
+        <h1 style={{color:'white'}}>Below are the monthly log</h1>
+        {data.blogPosts.map((post, index) => (
+          <div key={index}>
+            <h2 style={{color:'green'}}>{post.article_title}</h2>
+            <p>{post.author_name}</p>
+            {post.published_date ? (
+              <p>{post.published_date.toLocaleString()}</p>
+            ) : (
+              <p>No published date available</p>
+            )}
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
