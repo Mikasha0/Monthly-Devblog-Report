5b1285ad8931bee7e1f06c6df97e7ad44222b1a2
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-27 14:28:18 +0545

    configuring and changing the seed.ts file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,9 +2,18 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function seed(){
+    const aniket = await db.user.create({
+        data: {
+          username: "aniket",
+          // this is a hashed version of "twixron"
+          passwordHash:
+            "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+        },
+      });
     await Promise.all(
         gettoDoList().map((todos)=>{
-            return db.blog.create({data:todos});
+            const data = { blogId: aniket.id, ...todos };
+      return db.blog.create({ data });
         })
     )
 }
