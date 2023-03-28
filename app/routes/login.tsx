cd4b8012306b707aa9a80cfc9ec486de9655aa9d
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-28 09:34:23 +0545

    added new file called session.server.ts

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,4 @@
 import type { ActionArgs, LinksFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
 import { Link, useActionData, useSearchParams } from "@remix-run/react";
 
 import stylesUrl from "~/styles/login.css";
