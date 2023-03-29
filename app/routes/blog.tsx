bce48a10f41b5477d74df4f692c65be7be171db4
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-29 09:58:37 +0545

    redirecting to login page if the user is not logged in

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -4,6 +4,7 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import stylesUrl from "~/styles/blog.css";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -11,10 +12,12 @@ export const links: LinksFunction = () => {
 
 export const loader = async () => {
   return json({
-    blogs: await db.blog.findMany({take: 5,
-      select: { id: true,author_name:true },
+    blogs: await db.blog.findMany({
+      take: 5,
+      select: { id: true, author_name: true },
       distinct: ["author_name"],
-      orderBy: { author_name: "asc" },}),
+      orderBy: { author_name: "asc" },
+    }),
   });
 };
 
@@ -25,11 +28,7 @@ export default function BlogsRoute() {
       <header className="todos-header">
         <div className="container">
           <h1 className="home-link">
-            <Link
-              to="/"
-              title="Remix todos"
-              aria-label="Remix todos"
-            >
+            <Link to="/" title="Remix todos" aria-label="Remix todos">
               <span className="logo-medium">Monthly Devblog Reports</span>
             </Link>
           </h1>
@@ -41,10 +40,10 @@ export default function BlogsRoute() {
             <h3>Authors At Yarsa Labs</h3>
             <ul>
               {data.blogs.map((blog) => (
-              <li key={blog.id}>
-                <Link to={blog.id}>{blog.author_name}</Link>
+                <li key={blog.id}>
+                  <Link to={blog.id}>{blog.author_name}</Link>
                 </li>
-                ))}
+              ))}
             </ul>
             <Link to="postBlog" className="button">
               Add your own
